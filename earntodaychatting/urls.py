b222605945c3d17a72638edb9d339e96c3898603
faithfred-star <faithfred721@gmail.com>
2026-05-19 14:21:03 +0300

    Fixed admin.site.urls typo in routing config

--- earntodaychatting/urls.py
+++ earntodaychatting/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from accounts import views  # Make sure your accounts views are imported
+from accounts import views
 
 urlpatterns = [
-    path('admin/', admin.site.split),
+    # Change 'admin.site.split' to 'admin.site.urls'
+    path('admin/', admin.site.urls),  
+    
     path('', views.home, name='home'),
-    # --- ADD THIS LINE RIGHT HERE ---
     path('register/', views.register, name='register'), 
     path('verify-payment/', views.verify_payment, name='verify_payment'),
     path('earning/', views.dashboard, name='dashboard'),
