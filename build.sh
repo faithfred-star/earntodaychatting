4b144593137b89a6af2bd86ef01c5f83fb8acfae
faithfred-star <faithfred721@gmail.com>
2026-05-22 17:18:22 +0300

    Include automatic database migrations inside build.sh script

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
+# Install required packages
 pip install -r requirements.txt
 
+# Collect static files (if you have static assets configured)
 python manage.py collectstatic --no-input
+
+# Run migrations automatically to fix the missing column column error
 python manage.py migrate
